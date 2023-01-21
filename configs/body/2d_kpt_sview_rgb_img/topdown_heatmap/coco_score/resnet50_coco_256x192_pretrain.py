fcd6323f00b64cb17d002d3486b1d4c447d98ddc
Zhongyu Jiang <zyjiang@uw.edu>
2023-01-21 00:04:10 +0000

    Fix deconv layers for ResNet50

--- configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco_score/resnet50_coco_256x192_pretrain.py
+++ configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco_score/resnet50_coco_256x192_pretrain.py
@@ -36,7 +36,6 @@ model = dict(
         type='TopdownHeatmapScoreHead',
         in_channels=2048,
         out_channels=channel_cfg['num_output_channels'],
-        num_deconv_layers=0,
         extra=dict(
             final_conv_kernel=3, 
             num_conv_layers=4, 
